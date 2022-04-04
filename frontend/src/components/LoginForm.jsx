c224c4d99997b46fa671aa59d9d67aec6c15e26d
Mahi <starexify@gmail.com>
2022-04-05 00:38:09 +1000

    Track new files

---
+++ frontend/src/components/LoginForm.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { loginUser } from '../App';
+
+export function LoginForm () {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (
+    <>
+      <input onChange={event => setEmail(event.target.value)} type="text" placeholder='Email'/><br/>
+      <input onChange={event => setPassword(event.target.value)} type="password" placeholder='Password'/><br/>
+      <button onClick={() => loginUser(email, password)}>Login</button>
+    </>
+  );
+}
+
+export default LoginForm;
