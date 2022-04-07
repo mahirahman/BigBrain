ffc7d3b795cdad634fe4de93ad5c3c28ba64b928
Mahi <starexify@gmail.com>
2022-04-07 20:16:06 +1000

    Completed Nav Bar
    Restructured some files (Favicon)
    
    Installed React Icon library for icons instead of downloading individual
    SVGs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App () {
           <Route path='/quizzes' element={<Quizzes />} />
         </Routes>
       </BrowserRouter>
-      <svg viewBox="0 0 1440 320">
+      <svg className='wave_background_img' viewBox="0 0 1440 320">
         <path fill="#0099ff" fillOpacity="1" d="M0,192L80,208C160,224,320,256,480,245.3C640,235,800,181,960,165.3C1120,149,1280,171,1360,181.3L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
       </svg>
     </>
--- frontend/src/component/NavigationMenu.jsx
+++ frontend/src/component/NavigationMenu.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
-import logo from '../icons/logo_brain.png';
+import { useNavigate } from 'react-router-dom';
+import logo from '../img/logo_brain.png';
 import style from '../css/NavigationMenu.module.css';
+import { IoGameControllerOutline } from 'react-icons/io5';
+import { BsPlusCircle } from 'react-icons/bs';
+import { BiLogOut } from 'react-icons/bi';
 
 export function NavigationMenu () {
+  const navigate = useNavigate();
   return (
     <>
-      <div className={style.menu}>
-        <div className={style.logo}>
-          <img src={logo} alt="BigBrain Logo" className={style.logo_icon} />
-          <h1 className={style.logo_text}>BigBrain</h1>
+      <nav className={style.menu}>
+        <img src={logo} alt="BigBrain Logo" className={style.logo_icon} onClick={() => { navigate('/quizzes') }}/>
+        <div className={style.menu_links}>
+          <div onClick={() => { navigate('/quizzes') }}>
+            <IoGameControllerOutline/>
+            <div>View All Games</div>
+          </div>
+          <div onClick={() => { console.log('open new game modal') }}>
+            <BsPlusCircle/>
+            <div>Create New Game</div>
+          </div>
+          <div onClick={() => { navigate('/logout') }}>
+            <BiLogOut/>
+            <div>Logout</div>
+          </div>
         </div>
-      </div>
+      </nav>
     </>
   );
 }
