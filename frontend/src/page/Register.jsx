8f8ff968dc6b1bb0d7936a0eb955e05826c75f1c
Xiaotian Liu <cblxxlx@gmail.com>
2022-04-08 13:50:56 +1000

    add css design

--- frontend/src/page/Register.jsx
+++ frontend/src/page/Register.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import RegisterForm from '../component/RegisterForm';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import style from '../css/LoginRegisterForm.module.css';
 
 export function Register () {
   const navigate = useNavigate();
   return (
     <>
-      <div className="center">
-        <h1>Register your Account</h1>
+      <div className={`${style.form_container} ${style.center}`}>
+        <h1 className={style.form_main_title}>Register your Account</h1>
         <RegisterForm
           success={() => {
             navigate('/quizzes');
