4236e63de512adede1072c1f2b09d7b323cb3dd6
Mahi <starexify@gmail.com>
2022-04-21 03:01:49 +1000

    fixed filename

--- frontend/src/component/LineChart.jsx
+++ frontend/src/component/LineChart.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Line } from 'react-chartjs-2'
 import 'chart.js/auto';
-import style from '../css/Charts.module.css';
+import style from '../css/charts.module.css';
 
 export function LineChart (props) {
   LineChart.propTypes = {
