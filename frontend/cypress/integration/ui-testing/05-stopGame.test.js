8bab3f8d10c9bda633b074b9efa1b0740fdbcd3a
Xiaotian Liu <cblxxlx@gmail.com>
2022-04-21 07:05:38 +1000

    fixed cypress testing

--- frontend/cypress/integration/ui-testing/05-stopGame.test.js
+++ frontend/cypress/integration/ui-testing/05-stopGame.test.js
@@ -7,10 +7,8 @@ describe('UI testing', () => {
     cy.get('input[placeholder="Email"]').focus().type('jane.doe@gmail.com');
     cy.get('input[placeholder="Password"]').focus().type('password');
     cy.get('button[type=button]').click();
-    cy.get('.btn-outline-secondary').click();
-    cy.get('.btn-danger').click();
     // Manually end previous quiz from 04-startGame.test.js
-    cy.get('.btn-outline-secondary').click();
+    cy.get('.btn-outline-dark').click();
     // it should pop up an alert
     cy.on('window:alert', (text) => {
       expect(text).to.contains('Quiz has no active session');
